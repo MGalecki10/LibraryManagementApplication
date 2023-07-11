f3247faf6698d27790cb0dd737d6e6010f59b848
Mateusz Gałecki <m.galecki10@gmail.com>
2023-07-12 00:56:45 +0200

    Add exception

---
+++ src/main/java/pl/MGalecki10/Library/exception/NoSuchOptionException.java
@@ -0,0 +1,7 @@
+package pl.MGalecki10.Library.exception;
+
+public class NoSuchOptionException extends Exception {
+    public NoSuchOptionException(String message) {
+        super(message);
+    }
+}
