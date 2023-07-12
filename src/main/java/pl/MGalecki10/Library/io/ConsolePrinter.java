f41ec1da012864589629c88c02a45ca47f30b0a7
Mateusz Gałecki <m.galecki10@gmail.com>
2023-07-13 00:18:55 +0200

    Add class

---
+++ src/main/java/pl/MGalecki10/Library/io/ConsolePrinter.java
@@ -0,0 +1,36 @@
+package pl.MGalecki10.Library.io;
+
+import pl.MGalecki10.Library.model.Book;
+import pl.MGalecki10.Library.model.Magazine;
+import pl.MGalecki10.Library.model.Publication;
+
+public class ConsolePrinter {
+
+    public void printBooks(Publication[] publications) {
+        int counter = 0;
+        for (Publication publication : publications) {
+            if(publication instanceof Book) {
+                printLine(publication.toString());
+                counter++;
+            }
+        }
+        if (counter == 0)
+            printLine("Brak książek w bibliotece");
+    }
+
+    public void printMagazines(Publication[] publications) {
+        int counter = 0;
+        for (Publication publication : publications) {
+            if(publication instanceof Magazine) {
+                printLine(publication.toString());
+                counter++;
+            }
+        }
+        if (counter == 0)
+            printLine("Brak magazynów w bibliotece");
+    }
+
+    public void printLine(String text) {
+        System.out.println(text);
+    }
+}
