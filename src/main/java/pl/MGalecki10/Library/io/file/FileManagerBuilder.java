51c3367116ae4ab6880527f39283dd5eab1ea41b
Mateusz Gałecki <m.galecki10@gmail.com>
2023-07-24 23:12:35 +0200

    Add FileManagerBuilder

---
+++ src/main/java/pl/MGalecki10/Library/io/file/FileManagerBuilder.java
@@ -0,0 +1,49 @@
+package pl.MGalecki10.Library.io.file;
+
+import pl.MGalecki10.Library.exception.NoSuchFileTypeException;
+import pl.MGalecki10.Library.io.ConsolePrinter;
+import pl.MGalecki10.Library.io.DataReader;
+
+public class FileManagerBuilder {
+    private ConsolePrinter printer;
+    private DataReader reader;
+
+    public FileManagerBuilder(ConsolePrinter printer, DataReader reader) {
+        this.printer = printer;
+        this.reader = reader;
+    }
+
+    public FileManager build() {
+        printer.printLine("Wybierz format danych");
+        FIleType fileType = getFileType();
+        switch (fileType) {
+            case SERIAL:
+                return new SerializableFileManager();
+            default:
+                throw new NoSuchFileTypeException("Nieobsługiwany typ danych");
+        }
+
+    }
+
+    private FIleType getFileType() {
+        boolean typeOk = false;
+        FIleType result = null;
+        do {
+            printTypes();
+            String type = reader.getString().toUpperCase();
+            try {
+                result = FIleType.valueOf(type);
+                typeOk = true;
+            } catch (IllegalArgumentException e) {
+                printer.printLine("Nieobsługiwany typ danych, wybierz ponownie.");
+            }
+        } while (!typeOk);
+        return result;
+    }
+
+    private void printTypes() {
+        for (FIleType value : FIleType.values()) {
+            printer.printLine(value.name());
+        }
+    }
+}
