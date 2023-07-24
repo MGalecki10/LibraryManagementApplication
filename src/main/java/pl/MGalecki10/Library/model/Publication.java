de1d1877d7bad92bfbc8dbe1d6f0858dc1931923
Mateusz Gałecki <m.galecki10@gmail.com>
2023-07-24 23:13:37 +0200

    implements Serializable to class

--- src/main/java/pl/MGalecki10/Library/model/Publication.java
+++ src/main/java/pl/MGalecki10/Library/model/Publication.java
@@ -1,8 +1,9 @@
 package pl.MGalecki10.Library.model;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public abstract class Publication {
+public abstract class Publication implements Serializable {
     private String title;
     private String publisher;
     private int year;
