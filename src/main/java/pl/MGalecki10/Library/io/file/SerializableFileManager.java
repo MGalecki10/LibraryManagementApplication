6cc83310e6103fc2f84416bf314a329d72fac7a6
Mateusz Gałecki <m.galecki10@gmail.com>
2023-07-24 23:10:30 +0200

    Add Seriazable class

---
+++ src/main/java/pl/MGalecki10/Library/io/file/SerializableFileManager.java
@@ -0,0 +1,40 @@
+package pl.MGalecki10.Library.io.file;
+
+import pl.MGalecki10.Library.exception.DataExportException;
+import pl.MGalecki10.Library.exception.DataImportException;
+import pl.MGalecki10.Library.model.Library;
+import java.io.*;
+
+public class SerializableFileManager implements FileManager {
+    private static final String FILE_NAME = "Library.o";
+
+    @Override
+    public Library importData() {
+        try (
+                FileInputStream fis = new FileInputStream(FILE_NAME);
+                ObjectInputStream ois = new ObjectInputStream(fis);
+        ) {
+            return (Library) ois.readObject();
+        } catch (FileNotFoundException e) {
+            throw new DataImportException("Brak pliku " + FILE_NAME);
+        } catch (IOException e) {
+            throw new DataImportException("Błąd odczytu pliku " + FILE_NAME);
+        } catch (ClassNotFoundException e) {
+            throw new DataImportException("Niezgodny typ danych w pliku " + FILE_NAME);
+        }
+    }
+
+    @Override
+    public void exportData(Library library) {
+        try (
+                FileOutputStream fos = new FileOutputStream(FILE_NAME);
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+        ) {
+            oos.writeObject(library);
+        } catch (FileNotFoundException e) {
+            throw new DataExportException("Brak pliku " + FILE_NAME);
+        } catch (IOException e) {
+            throw new DataExportException("Błąd zapisu danych do pliku " + FILE_NAME);
+        }
+    }
+}
