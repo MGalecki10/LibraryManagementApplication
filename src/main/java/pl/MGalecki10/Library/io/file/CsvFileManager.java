79e1a4968c7b817226a9f4cc451acabb81e7518c
Mateusz Gałecki <m.galecki10@gmail.com>
2023-08-02 21:48:16 +0200

    Add method createObjectFromString() and update importData method

--- src/main/java/pl/MGalecki10/Library/io/file/CsvFileManager.java
+++ src/main/java/pl/MGalecki10/Library/io/file/CsvFileManager.java
@@ -1,25 +1,48 @@
 package pl.MGalecki10.Library.io.file;
 
 import pl.MGalecki10.Library.exception.DataExportException;
+import pl.MGalecki10.Library.exception.DataImportException;
+import pl.MGalecki10.Library.exception.InvalidDataException;
+import pl.MGalecki10.Library.model.Book;
 import pl.MGalecki10.Library.model.Library;
+import pl.MGalecki10.Library.model.Magazine;
 import pl.MGalecki10.Library.model.Publication;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class CsvFileManager implements FileManager {
     private static final String FILE_NAME = "Library.csv";
+
     @Override
     public Library importData() {
-        return null;
+        Library library = new Library();
+        try (Scanner fileReader = new Scanner(new File(FILE_NAME))) {
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                Publication publication = createObjectFromString(line);
+            }
+        } catch (FileNotFoundException e) {
+            throw new DataExportException("Brak pliku " + FILE_NAME);
+        }
+    }
+
+    private Publication createObjectFromString(String line) {
+        String[] split = line.split(";");
+        String type = split[0];
+        if (Book.TYPE.equals(type)) {
+            return createBook(split);
+        } else if (Magazine.TYPE.equals(type)) {
+            return createMagazine(split);
+        }
+        throw new InvalidDataException("Nieznany typ publikacji " + type);
     }
 
     @Override
     public void exportData(Library library) {
         Publication[] publications = library.getPublications();
         try (var fileWriter = new FileWriter(FILE_NAME);
-        var bufferedWriter = new BufferedWriter(fileWriter)) {
+             var bufferedWriter = new BufferedWriter(fileWriter)) {
             for (Publication publication : publications) {
                 bufferedWriter.write(publication.toCsv());
                 bufferedWriter.newLine();
