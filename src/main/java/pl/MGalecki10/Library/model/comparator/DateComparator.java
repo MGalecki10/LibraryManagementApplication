09d570fc7c316aea356fa474b3c89f76d150a6ca
Mateusz Gałecki <m.galecki10@gmail.com>
2023-08-25 12:45:04 +0200

    Add DateComparator

---
+++ src/main/java/pl/MGalecki10/Library/model/comparator/DateComparator.java
@@ -0,0 +1,21 @@
+package pl.MGalecki10.Library.model.comparator;
+
+import pl.MGalecki10.Library.model.Publication;
+
+import java.util.Comparator;
+
+public class DateComparator implements Comparator<Publication> {
+    @Override
+    public int compare(Publication p1, Publication p2) {
+        if (p1 == null && p2 == null) {
+            return 0;
+        } else if (p1 == null) {
+            return 1;
+        } else if (p2 == null) {
+            return -1;
+        }
+        Integer i1 = p1.getYear();
+        Integer i2 = p2.getYear();
+        return -i1.compareTo(i2);
+    }
+}
