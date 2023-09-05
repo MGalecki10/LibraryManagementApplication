54de42278dcb8b1526416735a25132262a6f5050
Mateusz Gałecki <m.galecki10@gmail.com>
2023-09-05 23:22:53 +0200

    Update variable 'year' from primitive data type (int) to non-primitive (Year)

--- src/main/java/pl/MGalecki10/Library/model/Publication.java
+++ src/main/java/pl/MGalecki10/Library/model/Publication.java
@@ -1,17 +1,18 @@
 package pl.MGalecki10.Library.model;
 
 import java.io.Serializable;
+import java.time.Year;
 import java.util.Objects;
 
 public abstract class Publication implements Serializable, Comparable<Publication>, CsvConvertible {
     private String title;
     private String publisher;
-    private int year;
+    private Year year;
 
     Publication(String title, String publisher, int year) {
         this.title = title;
         this.publisher = publisher;
-        this.year = year;
+        this.year = Year.of(year);
     }
 
     public String getTitle() {
@@ -30,11 +31,12 @@ public abstract class Publication implements Serializable, Comparable<Publicatio
         this.publisher = publisher;
     }
 
-    public int getYear() {
+
+    public Year getYear() {
         return year;
     }
 
-    public void setYear(int year) {
+    public void setYear(Year year) {
         this.year = year;
     }
 
@@ -43,14 +45,13 @@ public abstract class Publication implements Serializable, Comparable<Publicatio
         return title + ", " + publisher + ", " + year;
     }
 
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Publication that = (Publication) o;
-        return year == that.year &&
-                Objects.equals(title, that.title) &&
-                Objects.equals(publisher, that.publisher);
+        return Objects.equals(title, that.title) && Objects.equals(publisher, that.publisher) && Objects.equals(year, that.year);
     }
 
     @Override
